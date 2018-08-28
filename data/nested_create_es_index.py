8b7c46a2a12582efb0f4f50b01131df290c45e27
Jose Reyna <jose.reyna.rb@gmail.com>
2018-08-28 11:54:02 -0400

    Trying HTTP auth on Bonsai ES

--- data/nested_create_es_index.py
+++ data/nested_create_es_index.py
@@ -11,7 +11,12 @@ from elasticsearch_dsl import Index
 import certifi
 
 # Set to 'localhost' for development purposes
-connections.create_connection(hosts=[os.environ['ES_HOST']], ca_certs=certifi.where(), timeout=20)
+connections.create_connection(
+    hosts=[os.environ['ES_HOST']], 
+    http_auth=(os.environ['ES_USER'], os.environ['ES_PASS']),
+    ca_certs=certifi.where(), 
+    timeout=20)
+
 es = Elasticsearch()
 ess = Search(using=es)
 
